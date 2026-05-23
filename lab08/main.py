d1c7d58ff380e98a401e5fca4280e38fa9c505af
XBocTuKk <144593385+eksbatuk@users.noreply.github.com>
2026-05-23 11:07:21 +0700

    Updated lab8

--- lab08/main.py
+++ lab08/main.py
@@ -67,21 +67,25 @@ class PoissonSimulationApp:
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.plot_frame)
         self.canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 
-    def generate_poisson(self, mu, n):
-        results = []
-        L = math.exp(-mu)
+    def generate_poisson(self, mu, n):            
         
-        for _ in range(n):
+        def inner(l_val):
+            current_time = 0.0
             k = 0
-            p = 1.0
             while True:
+                u = random.random()
+                while u == 0.0:
+                    u = random.random()
+
+                t = -math.log(u)
+                current_time += t
+
+                if current_time > mu:
+                    return k
                 k += 1
-                p *= random.random()
-                if p < L:
-                    break
-            results.append(k - 1)
-            
-        return np.array(results)
+        
+        return np.array([inner(mu) for _ in range(n)])
+                
 
     def run_simulation(self):
         try:
